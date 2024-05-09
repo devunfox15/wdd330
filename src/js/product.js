b8a5ce70f9aa912391ab14920851d2394d747bc7
ashjoy7 <ashjoy7@gmail.com>
2024-05-09 16:58:06 -0700

    team update

--- src/js/product.js
+++ src/js/product.js
@@ -1,16 +1,9 @@
-import { setLocalStorage } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
+// product.js
+import { getParam } from "./utils.mjs";
+import productDetails, { addToCart } from "./productDetail.mjs";
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const productId = getParam("product");
+productDetails(productId);
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// Add to Cart button event handler
+document.getElementById("addToCart").addEventListener("click", addToCart);
---
+++ src/js/productDetail.mjs
@@ -0,0 +1,37 @@
+import { findProductById } from "./productData.mjs";
+import { setLocalStorage } from "./utils.mjs";
+
+let currentProduct = {};
+
+function renderProductDetails() {
+  // Render product details in HTML
+  const productContainer = document.getElementById("product-details");
+
+  // Example rendering of product details
+  productContainer.innerHTML = `
+    <div>
+      <h2>${currentProduct.name}</h2>
+      <p>${currentProduct.description}</p>
+      <p>Price: $${currentProduct.price}</p>
+      <button id="addToCart" data-id="${currentProduct.id}">Add to Cart</button>
+    </div>
+  `;
+}
+
+async function productDetails(productId) {
+  // Find product by id
+  currentProduct = await findProductById(productId);
+  if (currentProduct) {
+    renderProductDetails();
+  } else {
+    console.error("Product not found.");
+  }
+}
+
+function addToCart() {
+  // Function to add product to cart
+  setLocalStorage("so-cart", currentProduct);
+}
+
+export default productDetails;
+export { addToCart };
