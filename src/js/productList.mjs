dcc9a1f92c78004d663973a88179fe54cbfdb83d
CaptainCarnivore <ausjensen0451@byui.edu>
2024-05-16 16:31:41 -0700

    Team Activity 3 complete

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,4 +1,5 @@
 import { getData } from "./productData.mjs";
+import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
     return `<li class="product-card">
@@ -11,21 +12,18 @@ function productCardTemplate(product) {
       <h2 class="card__name">${product.NameWithoutBrand}</h2>
       <p class="product-card__price">$${product.FinalPrice}</p></a>
     </li>`;
-  }   
-  
-function renderList(list, elem) {
-    const htmlStrings =  list.map(productCardTemplate);
-    elem.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
-}
+  }  
 
 async function productList(selector, category) {
     // get the element we will insert the list into from the selector
     const elem = document.querySelector(selector);
     // get the list of products 
     const products = await getData(category);
-    console.log(products);
+    // filter out the two unuseed tents
+    const newProducts = products.filter(item => item.Id !== "989CG" && item.Id !== "880RT") 
+    console.log(newProducts);
     // render out the product list to the element
-    renderList(products, elem);
+    renderListWithTemplate(productCardTemplate, elem, newProducts);
     
 }
 
