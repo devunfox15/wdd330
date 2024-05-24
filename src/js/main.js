edce2ecc196f01c1aac652cb8299e2354f10e396
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-05-23 18:07:26 -0600

    Complete team activity... pushed all code according to the team activity

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
 import productList from "./productList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter("../partials/header.html", "../partials/footer.html");
 
 productList(".product-list", "tents");
