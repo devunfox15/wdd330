7480ecd13ae1671403990320e2c8b679a8586b2e
CaptainCarnivore <ausjensen0451@byui.edu>
2024-06-08 12:33:54 -0700

    team6 fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
+        cart: resolve(__dirname, "src/product_pages/cart/index.html"),
+        checkout: resolve(__dirname, "src/product_pages/checkout/index.html"),
         product: resolve(__dirname, "src/product_pages/index.html"),
         productList: resolve(__dirname, "src/product-list/index.html"),
       },
